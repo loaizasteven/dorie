4d5de252af1a0edf41559205aeaea9ffd98761e8
Steven Loaiza <steven.loaiza@outlook.com>
2024-11-10 16:09:00 -0500

    Set up venv

---
+++ libs/dorie/virtualenv.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+REPO="dorie"
+DIRECTORY="dorie_env"
+
+function activate () {
+  source $HOME/git/$REPO/$DIRECTORY/bin/activate
+  # logging info
+  echo "Currently activated $VIRTUAL_ENV"
+  python --version
+  # pip installation
+  pip install --upgrade pip
+  echo "pip installation in quiet mode..."
+  pip install -q -r $HOME/git/$REPO/libs/$REPO/requirements.txt --upgrade-strategy only-if-needed
+}
+
+if [ -d "$HOME/git/$REPO/$DIRECTORY" ]; then
+    activate
+else
+    echo "Creating venv"
+    /usr/local/bin/python3 -m venv "$HOME/git/$REPO/$DIRECTORY"
+
+    activate
+fi
