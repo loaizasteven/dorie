6a26b55768485fc161fa730939b26ea26632c390
Steven Loaiza <steven.loaiza@outlook.com>
2024-11-18 23:44:33 -0500

    Initial transformer loader

--- libs/dorie/loader/transformer.py
+++ libs/dorie/loader/transformer.py
@@ -5,21 +5,65 @@ import json
 import sys
 import os
 
+from pydantic import BaseModel
+from .datatokenizer import MyDataset
+
+import torch
+
 filedir = os.path.dirname(__file__)
 sys.path.append(filedir)
 
-config = json.load(open(f'{filedir}/config.json'))
 
-def load_model():
-    "Load pre-trained model and tokenizer"
-    tokenizer = AutoTokenizer.from_pretrained(config['baseModel'])
-    model = AutoModelForSequenceClassification.from_pretrained(config['baseModel'])
-    model.to(config['device'])
-    return model, tokenizer
+class ModelTrainer(BaseModel):
+    baseModel: str
+    modelArgs: dict
+    device: str
+    data: MyDataset
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.baseModel, num_labels=data.numLabels)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.baseModel)
+
+    def train(self, data):
+        training_args = TrainingArguments(**self.modelArgs)
+
+        trainer = Trainer(
+            model=self.model,
+            args=training_args,
+            train_dataset=data['train'],
+            eval_dataset=data['test'],
+            compute_metrics=lambda pred: {'accuracy': (pred.predictions.argmax(-1) == pred.label_ids).mean()}
+        )
+
+        trainer.train()
+
+    def save(self):
+        self.model.save_pretrained(self.outputDir)
+        self.tokenizer.save_pretrained(self.outputDir)
+
+    def predict(self, text):
+        inputs = self.tokenizer(text, return_tensors='pt')
+        with torch.no_grad():
+            logits = self.model(**inputs).logits
+        predicted_class = torch.argmax(logits)
+        return predicted_class
+
+    def evaluate(self, data):
+        trainer = Trainer(
+            model=self.model,
+            args=TrainingArguments(
+                per_device_eval_batch_size=self.batchSize
+            ),
+            eval_dataset=data,
+            compute_metrics=lambda pred: {'accuracy': (pred.predictions.argmax(-1) == pred.label_ids).mean()}
+        )
+
+        return trainer.evaluate()
+
 
 if __name__ == '__main__':
-    model, tokenizer = load_model()
-    print(model)
-    print(tokenizer)
-    print(config)
+    config = json.load(open(f'{filedir}/config.json'))
+    trainer = ModelTrainer(**config)
+    model = trainer.model()
     print('Model loaded successfully')
