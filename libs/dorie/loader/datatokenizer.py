d9cfa4b60c3f5b9d11016b8a4e5e3d2eb5ceab01
Steven Loaiza <steven.loaiza@outlook.com>
2024-11-19 21:35:47 -0500

    Update param to camelCase

--- libs/dorie/loader/datatokenizer.py
+++ libs/dorie/loader/datatokenizer.py
@@ -18,11 +18,11 @@ class MyDataset(BaseModel):
     labelmap: Optional[dict] = None
     split: float = 0.8
     pretrained_model_name: str = 'roberta-base'
-    num_labels: Optional[int] = None
+    numLabels: Optional[int] = None
 
     def _setnumlabels(self, dataset: Dataset):
         """Set the number of labels in the dataset"""
-        self.num_labels = len(dataset['label'].unique())
+        self.numLabels = len(dataset['label'].unique())
 
     def _fileformat(self):
         """Return the file format of the dataset"""
