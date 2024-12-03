1fa71a00ea039462b5e21b63fa28ce40ffdf647e
Steven Loaiza <steven.loaiza@outlook.com>
2024-12-02 22:05:11 -0500

    Add inference call to test loaded model

--- libs/dorie/intent/finetune.py
+++ libs/dorie/intent/finetune.py
@@ -5,6 +5,7 @@ from intent import Commons, config
 
 from loader import MyDataset, ModelTrainer
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import torch
 
 from typing import Optional, Union
 from pydantic import BaseModel
@@ -33,6 +34,7 @@ class Intent(BaseModel):
     config: dict = config()
     dataclass: Optional[MyDataset] = None
     trainer: Optional[Union[ModelTrainer, str]] = None
+    inference_text: Optional[str] = None
 
     def model_post_init(self, *args, **kwargs) -> None:
         """Override this method to perform additional initialization after `__init__` and `model_construct`.
@@ -41,6 +43,10 @@ class Intent(BaseModel):
         if isinstance(self.trainer, str):
             logger.info(f"Instance trainer is a string, loading model from path {self.trainer}")
             self._load_local_model(self.trainer)
+
+            logger.info("Testing inference on loaded model")
+            _, predicted_label = self._inference_call(self.inference_text)
+            logger.info(f"Input: {self.inference_text}: \n Predicted label: {predicted_label}")
 
     def _load_local_model(self, model_path: str) -> None:
         """Load a model from a local path."""
@@ -65,11 +71,21 @@ class Intent(BaseModel):
         model_object.push_to_hub(model_name)
         logger.info(f"Model saved to Hugging Face Hub as {model_name}")
 
+    def _inference_call(self, text: str):
+        inputs = self.config['tokenizer'](text, return_tensors='pt')
+        with torch.no_grad():
+            logits = self.config['model'](**inputs).logits
+
+        predicted_class = torch.argmax(logits)
+        predicted_label = self.config['model'].config.id2label.get(predicted_class.item())
+        return text, predicted_label
+
 
 if __name__ == '__main__':
     intent_classifier = Intent(
         datapath= osp.join(Commons().fileDir, 'samples/personal-auto-insurance-intents.csv'),
-        trainer=osp.join(Commons().fileDir, 'best_results_save')
+        trainer=osp.join(Commons().fileDir, 'best_results_save'),
+        inference_text="I was in a car accident and need to file a claim but I don't know how to start the process."
         )
     # intent_classifier.train()
     # intent_classifier.save('./results')
