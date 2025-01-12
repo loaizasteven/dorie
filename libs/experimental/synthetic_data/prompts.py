78d5527fa583269967c1c3dbfd63eb969fcea57b
Steven Loaiza <steven.loaiza@outlook.com>
2025-01-12 12:06:09 -0500

    Update prompt

--- libs/experimental/synthetic_data/prompts.py
+++ libs/experimental/synthetic_data/prompts.py
@@ -2,7 +2,7 @@ SYNTHETIC_FEW_SHOT_PREFIX = '''
     If you do not provide 2000 random examples for the following task, you will fail!
     Generate synthetic data for a personal auto car insurance intent such as saleQuote, addDriver, payPrem, 
     startClaim. You are restricted to the labels provided in this example, the text should be conversation utterance
-    for interactions between a customer and an insurance agent. Additionally, include some examples that are not related to the labels provided but still potential insurance related utterances, these examples should be classified as 'noIntent'.
+    for interactions between a customer and an insurance agent.
     '''
 
 USER_PROMPT = "YOU MUST PROVIDE 2000 EXAMPLES, DO NOT FAIL!"
