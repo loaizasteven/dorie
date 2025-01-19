36918b3c378e622fdf06c807c51b171dda7dd3aa
Steven Loaiza <steven.loaiza@outlook.com>
2025-01-19 16:47:12 -0500

    Add peft lora function

---
+++ libs/dorie/loader/adaptation.py
@@ -0,0 +1,33 @@
+#  ------------------------------------------------------------------------------------------
+#  Leverage huggingface Parameter-Efficient Fine Tuning Library
+#  Default behavior utilizes LORA: Low Rank Adaptation Method for fine tuning
+#  ------------------------------------------------------------------------------------------
+from peft import get_peft_config, get_peft_model, LoraConfig, TaskType
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+def return_peft_model(
+        model_name_or_path:str, 
+        lora_config:dict = {
+            "task_type": TaskType.SEQ_CLS, 
+            "inference_mode": False, 
+            "r": 8, 
+            "lora_alpha": 32, 
+            "lora_dropout": 0.1
+        }, 
+        verbose:bool=False
+    ):
+    """Return the PEFT model"""
+    peft_config = LoraConfig(**lora_config)
+
+    model = AutoModelForSequenceClassification.from_pretrained(model_name_or_path)
+    model = get_peft_model(model, peft_config)
+    if verbose:
+        logger.info(model.print_trainable_parameters())
+
+
+if __name__ == "__main__":
+    return_peft_model("bert-base-uncased", "bert-base-uncased")
